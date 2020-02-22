277429b84e1b758978b62f2d72dd82e12a8275a1
XnorTheUnforgiven <x.mg@videotron.ca>
2020-02-22 13:16:51 -0500

    Add json lib

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,12 @@ Last edit:          21-02-2020
 */
 
 #include <iostream>
+#include <lib\nlohmann\json.hpp>
 
 #include "include\distributech\cli.h"
+
+// for convenience
+using json = nlohmann::json;
  
 int main() {
   try {
