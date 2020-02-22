4951fc4ef5644029fee5faff8d164cb5cc253f76
XnorTheUnforgiven <x.mg@videotron.ca>
2020-02-21 20:13:46 -0500

    Basic arborescence

---
+++ project/main.cpp
@@ -0,0 +1,15 @@
+/********************************************************************
+Name: main.cpp
+Description:
+Author
+Date of creation:
+Last edit:
+*********************************************************************/
+
+#include <iostream>
+ 
+int main() {
+  std::cout << "Hello World\n";
+  int bob = 3;
+  getchar();
+}
